3cbef21cb39020f4242e2b523d0bd5b7ac6dada1
Mikhail <msryzhov@gmail.com>
2018-04-19 17:15:30 +0200

    -

---
+++ Booking/src/main/java/com/miage/hotel/roomBooked.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miage.hotel;
+
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+/**
+ *
+ * @author mikhail
+ */
+public class roomBooked {
+    @Id
+    private String id;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+    @DBRef
+    private String RoomID;
+
+    public roomBooked(LocalDateTime checkIn, LocalDateTime checkOut, String RoomID) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.RoomID = RoomID;
+    }
+    
+}
