b99ca042d286f30a0119f4b8426cda098c36c6d4
Mikhail <msryzhov@gmail.com>
2018-04-19 18:20:46 +0200

    Add hotel repo and create examples

--- Booking/src/main/java/com/miage/Application.java
+++ Booking/src/main/java/com/miage/Application.java
@@ -11,8 +11,15 @@ import com.miage.repositories.BookingRepository;
 import com.miage.models.Booking;
 import com.miage.models.Customer;
 import com.miage.models.Flight;
+import com.miage.models.Hotel;
+import com.miage.models.Room;
+import com.miage.models.RoomType;
 import com.miage.models.Traveller;
+import com.miage.models.roomBooked;
+import com.miage.repositories.BookedRoomRepository;
 import com.miage.repositories.FlightRepository;
+import com.miage.repositories.HotelRepository;
+import com.miage.repositories.RoomRepository;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -45,7 +52,16 @@ public class Application implements CommandLineRunner {
 
     @Autowired
     private FlightRepository repositoryFlight;
-
+    
+    @Autowired
+    private HotelRepository repositoryHotel;
+    
+    @Autowired
+    private BookedRoomRepository repositoryBookedRoom;
+    
+    @Autowired
+    private RoomRepository repositoryRoom;
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -57,6 +73,10 @@ public class Application implements CommandLineRunner {
         repositoryFlight.deleteAll();
         repositoryTraveller.deleteAll();
         repositoryBooking.deleteAll();
+        repositoryHotel.deleteAll();
+        repositoryBookedRoom.deleteAll();
+        repositoryRoom.deleteAll();
+                
 
         // save a couple of customers
         repository.save(new Customer("Alice", "Smith", "1245"));
@@ -118,7 +138,28 @@ public class Application implements CommandLineRunner {
         b1.setTravellers(liT);
 
         repositoryBooking.save(b1);
-
+        
+        //Booking room part-----
+        Hotel hot1= new Hotel("town1 street1", "First", 12345, 2, "1@1.fr", "06");
+        Hotel hot2= new Hotel("town2 street2", "Second", 54321, 2, "2@2.fr", "07");
+        hot1 = repositoryHotel.save(hot1);
+        hot2 = repositoryHotel.save(hot2);
+        
+        Room room1 = new Room(RoomType.Double, 2,hot1);
+        Room room2 = new Room(RoomType.Single, 1,hot1);
+        Room room3 = new Room(RoomType.King, 5,hot2);
+        room1= repositoryRoom.save(room1);
+        room2= repositoryRoom.save(room2);
+        room3= repositoryRoom.save(room3);
+        hot1.addRoom(room1);
+        hot1.addRoom(room2);
+        hot2.addRoom(room3);
+        repositoryHotel.save(hot1);
+        repositoryHotel.save(hot2);                
+        roomBooked rb1= new roomBooked(LocalDateTime.of(2018, Month.MARCH, 1, 12, 0), 
+                LocalDateTime.of(2018, Month.MARCH, 2, 12, 0), room1);
+        repositoryBookedRoom.save(rb1);
+        //End Booking room part----
     }
 
 }
