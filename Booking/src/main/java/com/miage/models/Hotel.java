48c36dc05d23e311b2291f232c3ecc1b288b220c
Tamer <tamer.abdulghani@gmail.com>
2018-04-19 17:21:24 +0200

    hotels to models

--- Booking/src/main/java/com/miage/models/Hotel.java
+++ Booking/src/main/java/com/miage/models/Hotel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.miage.hotel;
+package com.miage.models;
 
 import java.util.ArrayList;
 import org.springframework.data.annotation.Id;
